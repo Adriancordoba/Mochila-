14491c4d3c211691f64a7330b77e81cb3f5fe8c0
Adriancordoba <51544377+Adriancordoba@users.noreply.github.com>
2019-06-09 16:29:20 -0600

    Update Mochila.cpp

--- Mochila.cpp
+++ Mochila.cpp
@@ -1,1 +1,9 @@
-
+#include "Mochila.h"
+Mochila(int, int *);
+Mochila();
+Criatura ** cruzar( Criatura *); 
+void mutar();
+ostream& imprimir(ostream&);
+Criatura * clonar(); 
+Criatura * inverso(); 
+int operator==(Criatura *);
