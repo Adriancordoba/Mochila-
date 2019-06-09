04758f282ea74ebd6bfd1e1d86d64a20575e46ce
Adriancordoba <51544377+Adriancordoba@users.noreply.github.com>
2019-06-09 16:27:33 -0600

    Update Mochila.h

--- Mochila.h
+++ Mochila.h
@@ -1,1 +1,19 @@
-
+#ifndef MOCHILA
+#define MOCHILA
+class Mochila : public Criatura{
+public:
+		int * piedras;
+		int cantPiedras;
+		Mochila(int, int *)
+		Mochila()
+		// Recibe: Un puntero a una criatura para cruzarla con la actual
+		// Efectua: Retorna un vector terminado en 0 con los hijos
+		Criatura ** cruzar( Criatura *); //tiene que borrarse
+		// Efecto: Aplica una mutación a la criatura
+		void mutar();
+		ostream& imprimir(ostream&);
+		Criatura * clonar(); //tiene que borrarse
+		Criatura * inverso(); //tiene que borrarse
+		int operator==(Criatura *); //
+};
+#endif
