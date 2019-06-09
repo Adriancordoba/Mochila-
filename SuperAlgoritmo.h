1a5c4616e3eff5aa371df2eb970f85e26ab499b7
Adriancordoba <51544377+Adriancordoba@users.noreply.github.com>
2019-06-09 16:17:12 -0600

    Create SuperAlgoritmo.h

---
+++ SuperAlgoritmo.h
@@ -0,0 +1,9 @@
+#ifndef SUPERALGORITMO
+#define SUPERALGORITMO
+#include "Algoritmo.h"
+#include "Poblacion.h"
+class SuperAlgoritmo : public Algoritmo {
+	  public:
+	  	Poblacion * evolucionar( Poblacion *);
+};
+#endif
